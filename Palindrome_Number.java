d2c557c865f4a45e3154200e785a814bb68ce293
dfrafelipe30 <daniel.rambaut@urosario.edu.co>
2020-12-30 08:18:48 -0500

    Palindrome Number

--- Palindrome_Number.java
+++ Palindrome_Number.java
@@ -1,6 +1,5 @@
-public class reverse_integer {
+public class Palindrome_Number {
     public static int reverse (int x){
-        
         long sol = 0;
         while (x != 0){
             sol = sol *10 + x %10;
@@ -15,9 +14,22 @@ public class reverse_integer {
         //sol = (negativo) ? (-1 * sol) : sol;
         return (int)sol;
     }
+    
+    public static boolean isPalindrome(int x){
+        if (x < 0){
+            return false;
+        }
+        int aux = reverse(x);
+        if (x == aux){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
     public static void main(String[] args) {
-        int x = -2147483648;
-        int sol = reverse(x);
-        System.out.print(sol);
+        int x = -121;
+        boolean sol = isPalindrome(x);
+        System.out.println(sol);
     }
 }
